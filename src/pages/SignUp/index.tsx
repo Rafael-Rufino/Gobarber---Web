1e122e6e15ce875b8651bf3a6143f2a935ea960c
RafaelRufino <rafaelrufino.52@gmail.com>
2021-11-30 01:58:52 -0300

    configurando o unform

--- src/pages/SignUp/index.tsx
+++ src/pages/SignUp/index.tsx
@@ -2,15 +2,19 @@ import React from 'react';
 import { Container, Content, Background } from './styles';
 import logoImg from '../../assets/logo.svg';
 import { FiArrowLeft, FiMail, FiLock, FiUser } from 'react-icons/fi';
+import { Form } from '@unform/web';
 import Input from '../../components/Input';
 import Button from '../../components/Button';
 const SignUp: React.FC = () => {
+  function handleSubmit(data: object): void {
+    console.log(data);
+  }
   return (
     <Container>
       <Background />
       <Content>
         <img src={logoImg} alt="Gobarber" />
-        <form>
+        <Form onSubmit={handleSubmit}>
           <h1>Faça seu cadastro</h1>
           <Input name="name" icon={FiUser} type="name" placeholder="Nome" />
           <Input name="email" icon={FiMail} type="email" placeholder="E-mail" />
@@ -23,7 +27,7 @@ const SignUp: React.FC = () => {
           />
 
           <Button type="submit">Cadastrar</Button>
-        </form>
+        </Form>
         <a href="/SignIn">
           <FiArrowLeft />
           Voltar para login
