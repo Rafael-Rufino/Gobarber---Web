2f19f18031b7e413b9c2e981e9c34acfb3e98a89
RafaelRufino <rafaelrufino.52@gmail.com>
2021-12-06 03:39:31 -0300

    finalizando tela de login

--- src/pages/SignIn/index.tsx
+++ src/pages/SignIn/index.tsx
@@ -1,5 +1,5 @@
-import React, { useCallback, useRef } from 'react';
-import { Container, Content, Background } from './styles';
+import React, { useCallback, useRef, useContext } from 'react';
+import { Container, Content, Background, AnimationContainer } from './styles';
 import logoImg from '../../assets/logo.svg';
 import { FormHandles } from '@unform/core';
 import { FiLogIn, FiMail, FiLock } from 'react-icons/fi';
@@ -7,50 +7,86 @@ import Input from '../../components/Input';
 import Button from '../../components/Button';
 import { Form } from '@unform/web';
 import * as Yup from 'yup';
+import { useAuth } from '../../hooks/AuthContext';
+import { useToast } from '../../hooks/ToastContext';
 import getValidationErrors from '../../utils/getValidationErrors';
+import { Link, useHistory } from 'react-router-dom';
+
+interface SignInFormData {
+  email: string;
+  password: string;
+}
 const SignIn: React.FC = () => {
   const formRef = useRef<FormHandles>(null);
 
-  const handleSubmit = useCallback(async (data: object) => {
-    try {
-      formRef.current?.setErrors({});
-      const schema = Yup.object().shape({
-        email: Yup.string()
-          .required('E-mail obrigatório')
-          .email('Digite um e-mail válido'),
-        password: Yup.string().required('Senha obrigatória'),
-      });
+  const { signIn } = useAuth();
+  const { addToast } = useToast();
+  const history = useHistory();
 
-      await schema.validate(data, {
-        abortEarly: false,
-      });
-    } catch (err: any) {
-      const errors = getValidationErrors(err);
-      formRef.current?.setErrors(errors);
-    }
-  }, []);
+  const handleSubmit = useCallback(
+    async (data: SignInFormData) => {
+      try {
+        formRef.current?.setErrors({});
+        const schema = Yup.object().shape({
+          email: Yup.string()
+            .required('E-mail obrigatório')
+            .email('Digite um e-mail válido'),
+          password: Yup.string().required('Senha obrigatória'),
+        });
+
+        await schema.validate(data, {
+          abortEarly: false,
+        });
+        await signIn({
+          email: data.email,
+          password: data.password,
+        });
+        history.push('/dashboard');
+      } catch (err) {
+        if (err instanceof Yup.ValidationError) {
+          const errors = getValidationErrors(err);
+          formRef.current?.setErrors(errors);
+          return;
+        }
+        addToast({
+          type: 'error',
+          title: 'Erro na autenticação',
+          description: 'Ocorreu um erro ao fazer login, cheque as credenciaias',
+        });
+      }
+    },
+    [signIn, addToast, history],
+  );
   return (
     <Container>
       <Content>
-        <img src={logoImg} alt="Gobarber" />
-        <Form ref={formRef} onSubmit={handleSubmit}>
-          <h1>Faça seu login</h1>
-          <Input name="email" icon={FiMail} type="email" placeholder="E-mail" />
-          <Input
-            name="password"
-            icon={FiLock}
-            type="password"
-            placeholder="Senha"
-          />
+        <AnimationContainer>
+          <img src={logoImg} alt="Gobarber" />
+          <Form ref={formRef} onSubmit={handleSubmit}>
+            <h1>Faça seu login</h1>
+            <Input
+              name="email"
+              icon={FiMail}
+              type="email"
+              placeholder="E-mail"
+            />
+            <Input
+              name="password"
+              icon={FiLock}
+              type="password"
+              placeholder="Senha"
+            />
 
-          <Button type="submit">Entrar</Button>
-          <a href="forgot">Esqueci minha senha</a>
-        </Form>
-        <a href="">
-          <FiLogIn />
-          Criar conta
-        </a>
+            <Button type="submit">Entrar</Button>
+            <a href="forgot">Esqueci minha senha</a>
+          </Form>
+          <Link to="/signup">
+            <FiLogIn />
+            Criar conta
+          </Link>
+        </AnimationContainer>
       </Content>
+
       <Background />
     </Container>
   );
--- src/pages/SignIn/styles.ts
+++ src/pages/SignIn/styles.ts
@@ -1,22 +1,27 @@
-import styled from 'styled-components';
+import styled, { keyframes } from 'styled-components';
 import SigInBackground from '../../assets/sign-in-background.png';
 export const Container = styled.div`
   height: 100vh;
   display: flex;
   align-items: stretch;
 `;
-export const Content = styled.div`
+
+export const appearFromLeft = keyframes`
+  from{
+    opacity: 0;
+    transform: translateX(-50px);
+  }
+  to{
+    opacity: 1;
+    transform: translateX(0)
+  }
+`;
+export const AnimationContainer = styled.div`
   display: flex;
-  place-content: center;
   flex-direction: column;
   align-items: center;
-  width: 100%;
-  max-width: 700px;
-
-  /* img {
-    width: 200px;
-    height: 200px;
-  } */
+  justify-content: center;
+  animation: ${appearFromLeft} 1s;
   form {
     margin: 80px 0;
     width: 340px;
@@ -56,6 +61,14 @@ export const Content = styled.div`
       filter: brightness(0.9);
     }
   }
+`;
+export const Content = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+  width: 100%;
+  max-width: 700px;
 `;
 export const Background = styled.div`
   flex: 1;
