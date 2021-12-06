9ec42d4bf3212633a61532a4b767051bd7db8a57
RafaelRufino <rafaelrufino.52@gmail.com>
2021-12-06 03:43:08 -0300

    criando os tipos de messagem de toasfay

--- src/components/Tooltip/index.tsx
+++ src/components/Tooltip/index.tsx
@@ -5,13 +5,15 @@ interface TooltipProps {
   title: string;
   className?: string;
 }
-const Tooltip: React.FC<TooltipProps> = ({ title, className, children }) => {
-  return (
-    <Container className={className}>
-      {children}
-      <span>{title}</span>
-    </Container>
-  );
-};
+const Tooltip: React.FC<TooltipProps> = ({
+  title,
+  className = '',
+  children,
+}) => (
+  <Container className={className}>
+    {children}
+    <span>{title}</span>
+  </Container>
+);
 
 export default Tooltip;
